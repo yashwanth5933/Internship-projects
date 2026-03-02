2413d1c8d0d9e43b413c9fa7cb100a16cb21d07a
Lavaraju Pasagadugula <lavarajupasagadugula5933@gmail.com>
2026-03-02 15:09:51 +0530

    Refactor house price prediction script

--- #house price prediction Regression coeff.py
+++ #house price prediction Regression coeff.py
@@ -1,33 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Regression coefficients
-b = 10        # intercept
-m1 = 1000     # area coefficient
-m2 = 4        # rooms coefficient
-m3 = 3        # age coefficient
-
-# Fixed input values
+b = 10
+m1 = 1000     
+m2 = 4       
+m3 = 3        
 rooms = 3
 age = 3
-
-# Create range of area values
 area_values = np.linspace(100, 1000, 100)
-
-# Calculate predicted prices
 price_values = b + m1 * area_values + m2 * rooms + m3 * age
-
-# Plot regression line
 plt.plot(area_values, price_values)
-
-# Highlight specific house (Area = 600)
 Area = 600
 Price = b + m1 * Area + m2 * rooms + m3 * age
 plt.scatter(Area, Price)
-
-# Labels
 plt.xlabel("Area")
 plt.ylabel("Predicted Price")
 plt.title("House Price Prediction using Linear Regression")
-
 plt.show() 
